57bed1304c6558c3cba7f72689847ea46d63a2e8
dibyaGodigit <139847390+dibyaGodigit@users.noreply.github.com>
2023-07-18 12:42:15 +0530

    Update TestingCode.java

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -3,8 +3,8 @@ package com.digit.testProj.p1;
 public class TestingCode {
 public static void main(String[] args) {
 	System.out.println("Hello GITHUB");
-	System.out.println("Hello GITHUB");
-	System.out.println("Hello GITHUB");
+	System.out.println("Hello GITHUB 2");
+	System.out.println("Hello GITHUB 3");
 	
 }
 }
